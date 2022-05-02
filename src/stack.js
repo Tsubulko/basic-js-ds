d49fddddd58cdcc958da9a5b0ca5bc9deb46ed67
Tsubulko <pashatsubulko@gmail.com>
2022-05-02 17:56:18 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() 
+  {
+    this.arr = [];
+  }
+  push(a) {
+    this.arr.push(a);
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  pop() 
+  {
+    return this.arr.pop();
   }
-
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  peek() 
+  {
+    return this.arr[this.arr.length - 1];
   }
 }
 
